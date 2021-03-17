e9071ec93847ca341e886b635948baab93b38665
Muath Nassar <moadnassar@gmail.com>
2021-03-17 13:23:51 -0700

    Second

--- app/src/main/java/com/example/newproject/MainActivity.kt
+++ app/src/main/java/com/example/newproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -11,10 +12,9 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val list = mutableListOf<String>()
     private lateinit var progressDialog: ProgressDialog
     private lateinit var db: FirebaseFirestore
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         progressDialog.setMessage("Uploading..!")
         progressDialog.setCancelable(false)
         //---------------------
+        fetchData()
         btnSend.setOnClickListener {
             if (
                 etName.text.isNotEmpty()
@@ -39,12 +40,26 @@ class MainActivity : AppCompatActivity() {
         progressDialog.show()
         db.collection("contacts").add(
             Contact(name,mobile,address)
-        ).addOnSuccessListener { 
+        ).addOnSuccessListener {
+            fetchData()
             progressDialog.dismiss()
             Toast.makeText(this,"Added successfully",Toast.LENGTH_LONG).show()
         }.addOnFailureListener {
             progressDialog.dismiss()
             Toast.makeText(this,"Something went Wrong",Toast.LENGTH_LONG).show()
         }
+    }
+    private fun fetchData(){
+        db.collection("contacts").get().addOnSuccessListener {
+            list.clear()
+            for (i in it){
+                val n = i.getString("name")
+                val m =i.getString("mobile")
+                list.add("$n  |  $m")
+            }
+            val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , list)
+            rvContacts.adapter = arrayAdapter
+        }
+
     }
 }
